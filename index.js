c5744036b137a1e638291a1c039be2b209b8ea0d
delvinalfredo <delvinalfredo@gmail.com>
2022-03-30 12:16:20 +0700

    adding index

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+// const stores = require('./routes/stores')
+
+// var bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+  res.send("hello world")
+})
+
+// app.use('/store', stores)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
